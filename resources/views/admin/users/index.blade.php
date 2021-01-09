3c95b8700284d197ebb0bd3b41ad97daf695d7a6
marouanetaki <yayaw2013@outlook.com>
2021-01-09 14:57:33 +0100

    walo

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 @section('content')
 <section class="content">
-    <div class="container-fluid mt-5">
+    <div class="container mt-5">
         
       <div class="row">
         <div class="col-md-12">
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::get('/', function () {
 
 
 /******************************************Admin Routes******************************************/
-
 Auth::routes(['verify' => true]);
 Route::get('/admin/dashboard', 'HomeController@index')->name('dashboard');
 Route::resource('/home/auteurs' , 'Admin\AuteursController', ['as'=>'home']);
@@ -28,11 +27,10 @@ Route::resource('/home/genres' , 'Admin\GenresController', ['as'=>'home']);
 Route::resource('/home/tages' , 'Admin\TagesController', ['as'=>'home']);
 Route::resource('/home/livres' , 'Admin\LivresController', ['as'=>'home']);
 //Route::resource('/home/emprunts' , 'Admin\EmpruntsController', ['as'=>'home']);
-
-/******************************************Admin Routes******************************************/
 Route::namespace('Admin')->prefix('home')->name('home.')->middleware('can:manage-users')->group(function() {
     Route::resource('users' , 'UsersController');
 });
 Route::namespace('Admin')->prefix('home')->name('home.')->group(function() {
     Route::resource('emprunts' , 'EmpruntsController');
 });
+/******************************************Admin Routes******************************************/
